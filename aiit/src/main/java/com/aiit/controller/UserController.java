64aa10f27b1f6ba94111aa702a63f9ddc8fb6d91
Bao <2475477449@qq.com>
2022-03-26 16:32:38 +0800

    SSm-shiro身份验证与权限校验v3.0

--- aiit/src/main/java/com/aiit/controller/UserController.java
+++ aiit/src/main/java/com/aiit/controller/UserController.java
@@ -12,9 +12,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @Controller
 public class UserController {
 
